6bc7514e2dceaf05866242ccef5b868e9cd225f4
reevetm <75334116+reevetm@users.noreply.github.com>
2021-01-23 22:55:49 -0500

    Add files via upload

---
+++ FUN STUFF.js
@@ -0,0 +1,118 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: light-brown; icon-glyph: magic;
+
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: light-gray; icon-glyph: microchip;
+
+let bgImage = await getImage("https://raw.githubusercontent.com/MisutaaUrufu/iOS-System-14-Theme/master/Light/Scriptable/graphics/Classic/Light/widget.png")
+let widget = await createWidget()
+
+// Check if the script is running in
+// a widget. If not, show a preview of
+// the widget to easier debug it.
+if (!config.runsInWidget) {
+  await widget.presentMedium()
+}
+
+// Tell the system to show the widget.
+Script.setWidget(widget)
+Script.complete()
+
+function buildInterface() {
+	let bg = bgImage
+	let context = new DrawContext()
+	
+	context.respectScreenScale = false
+    
+	let os = Device.systemName()
+	let ver = Device.systemVersion()
+	let dev = Device.model()
+	let name = Device.name()
+	let bStat = Device.batteryLevel().toFixed(2) * 100
+	let lang = Device.language() 
+	let loc = Device.locale()
+	let pResW=Device.screenResolution().width
+  	let pResH=Device.screenResolution().height
+  
+	let pSizeH=Device.screenSize().height
+	let pSizeW=Device.screenSize().width
+
+
+
+let pScale=Device.screenScale()
+QuickLook.present(pScale, false )
+
+	let pStat = "Charger is not connected."
+	
+	if (!Device.isDischarging() || Device.isCharging()) {
+		pStat = "Charger is connected."
+	}
+	
+	let rec = new Rect(0, 0, 1024, 482)
+	
+	context.size = new Size(1024, 482)
+	
+	if (bg != null) {
+		context.drawImageInRect(bg, rec)
+	}
+	
+	let charcoal = new Font("Charcoal", 32)
+	context.setFont(charcoal)
+	context.setTextColor(Color.black())
+	context.setTextAlignedLeft()
+	
+	let lText = "Language: " + lang.toUpperCase()
+	
+	
+	let vText = os + " version " + ver
+	let bText = "Battery Level: " + bStat.toFixed(0) + "%"
+// new
+    let sRes="Screen Res:  H: " + pResH + "  W: " + pResW
+
+      let sSize="Screen SZ:  H: " + pSizeH + "  W: " + pSizeW
+
+   let sScale="Screen Scale: " + pScale
+
+// end new 
+	
+	context.drawText(vText, new Point(692, 43))
+	context.drawText(dev, new Point(692, 91)) 
+//Point(692, 91)
+	
+	context.drawText(lText, new Point(692, 139))
+
+	context.drawText(name, new Point(527, 200))
+//Point(527, 139))
+
+// 	new stuff 
+context.drawText(sRes, new Point(527, 257))
+
+context.drawText(sSize,  new Point(527, 325))
+
+
+
+// end new
+	
+	context.drawText(bText, new Point(527, 357))
+	context.drawText(pStat, new Point(527, 405))
+	
+	let img = context.getImage()
+	return img
+}
+
+async function getImage(url) {
+	let req = new Request(url)
+	return await req.loadImage()
+}
+
+async function createWidget() {
+  let bg = buildInterface()
+  let w = new ListWidget()
+
+  if (bg != null) {
+    w.backgroundImage = bg
+  }
+  return w
+}
